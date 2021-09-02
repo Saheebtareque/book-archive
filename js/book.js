a97806de6bec45e3d7c06492b561da7679c3287d
saheeb_tareque <saheebtareque97@gmail.com>
2021-09-02 16:45:10 +0600

    data loading in the web page is done

--- js/book.js
+++ js/book.js
@@ -16,10 +16,24 @@ const showBooks = books =>
     bookResult = document.getElementById('book-result');
     bookResult.textContent = '';
     books.forEach(book => {
-        console.log(book.cover_i);
-        // const div = document.createElement('div');
-        // div.classList.add('col');
-        // div.innerHTML= ``;
+        // console.log(book.cover-i);
+       const cover_i = book.cover_i;
+       
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML= `<div class="card h-100">
+        <img style="height:350px"src="https://covers.openlibrary.org/b/id/${cover_i}-M.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <p class="card-text">Author: ${book.author_name}</p>
+          <p class="card-text">Published in: ${book.first_publish_year}</p>
+          <p class="card-text">Publisher: ${book.publisher}</p>
+
+        </div>
+      </div>`;
+      
+      bookResult.appendChild(div);
+
     })
 
 }
